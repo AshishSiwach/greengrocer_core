05e187867a8290dd0e6faf2b59d818a1c97b5e62
ashishsiwach2789 <as2095@exeter.ac.uk>
2026-01-25 00:04:25 +0000

    Add missing product_name column to stg_sales model

--- dbt_transform/models/staging/stg_sales.sql
+++ dbt_transform/models/staging/stg_sales.sql
@@ -17,7 +17,7 @@ SELECT
     -- 3. Fix Data Types (Casting String to Real Types)
     CAST(sale_date AS TIMESTAMP) as transaction_at,  -- <--- FIXED THIS
     CAST(product_id AS VARCHAR) as product_id,
-    CAST(product_name AS VARCHAR) as product_name
+    CAST(product_name AS VARCHAR) as product_name,
     CAST(quantity AS INTEGER) as quantity,
     CAST(unit_price AS DECIMAL(10,2)) as unit_price,
     CAST(total_amount AS DECIMAL(10,2)) as total_amount
